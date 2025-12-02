11f1cc1fe0fb2c8615f5403569f4c9f704859212
Omid Jabbari <omidjabbri0041@gmail.com>
2025-12-03 00:02:03 +0330

    add Farsi Fonts to project

---
+++ src/constants/localFont.ts
@@ -0,0 +1,66 @@
+import LocalFont from "next/font/local";
+
+const iranYekanFont = LocalFont({
+  src: [
+    {
+      path: "../../public/fonts/iranYekan/IRANYekanX-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranYekan/IRANYekanX-UltraLight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranYekan/IRANYekanX-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranYekan/IRANYekanX-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranYekan/IRANYekanX-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranYekan/IRANYekanX-DemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranYekan/IRANYekanX-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranYekan/IRANYekanX-ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranYekan/IRANYekanX-Heavy.woff2",
+      weight: "850",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranYekan/IRANYekanX-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranYekan/IRANYekanX-ExtraBlack.woff2",
+      weight: "950",
+      style: "normal",
+    },
+  ],
+  variable: "--font-iranYekan",
+  style: "normal",
+  display: "block",
+});
+
+export default iranYekanFont;
