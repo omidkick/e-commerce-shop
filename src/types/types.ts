89b4ae7732ed01cd42726784ffa64bf85938d3e1
Omid Jabbari <omidjabbri0041@gmail.com>
2025-12-04 18:52:27 +0330

    ProductsList component is initially added by defining its types in " type.ts" file.

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+// types for Single Product:
+export type ProductType = {
+  id: string | number;
+  name: string;
+  shortDescription: string;
+  description: string;
+  price: number;
+  sizes: string[];
+  colors: string[];
+  images: Record<string, string>;
+};
+
+// types for Multiple Products:
+export type ProductsType = ProductType[];
