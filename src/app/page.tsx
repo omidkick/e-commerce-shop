2f1151b8d60e27e48027a244484a0e4a44092188
Omid Jabbari <omidjabbri0041@gmail.com>
2025-12-04 13:54:17 +0330

    Banner is designed and added to Home page like a pro.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+
 const Homepage = () => {
   return (
     <div className="text-5xl">
-
+      <div className="relative aspect-3/1 mb-12 mt-10 ">
+        <Image src="/featured.png" alt="Featured product" fill className="" />
+      </div>
     </div>
   );
 };
